d3ba1652ae8b3851c3e9f14dda762d8e241be826
Kamran Masood <kam1088@gmail.com>
2022-03-07 21:28:55 -0800

    completed stateless components, refactoring App.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,24 +3,42 @@ import VideoPlayer from './VideoPlayer.js';
 import VideoList from './VideoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
+class App extends React.Component {
 
-var App = (props) => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search />
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      // define states to monitor
+
+    };
+  }
+
+  // add event handlers
+
+  render() {
+
+    // apply any state conditions here
+
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search />
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={exampleVideoData[0]} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={exampleVideoData} />
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer />
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={exampleVideoData} />
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
